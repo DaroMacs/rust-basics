665ae4feaa2a9d254a3d5df614633a6bf6145d1f
DaroMacs <dario.maceg@gmail.com>
2025-09-30 20:29:06 -0600

    Refactor main.rs: Comment out unused functions and add is_odd_number function for checking odd numbers.

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,58 @@
-const THISISA_CONSTANT: u32 = 100;
+// const THISISA_CONSTANT: u32 = 100;
 fn main() {
-    immutability_test();
-    scalar_types();
-    shadowing();
-    println!("The value of the constant is: {}", THISISA_CONSTANT);
+//   let factorial_result = factorial_calculation( 25);
+// println!("The factorial is: {}", factorial_result);
+
+let is_odd = is_odd_number(4);
+println!("Number is odd: {}", is_odd);
+
 }
 
-fn immutability_test() {
-    let x   = 1;   
-    println!("The value of x is: {}", x);
-    // x = 6; // This line would cause a compile-time error because x is immutable
+// fn factorial_calculation(number: u128) -> u128 {
+//     if number == 0 || number == 1 {
+//         1
+//     } else {
+//        let mut result = number;
+//        for i in (1..number).rev() {
+//            result *= i;
+//        } 
+//          result
+//     }
+        
+// }
 
-    let mut y = 10; // y is mutable
-    println!("The value of y is: {}", y);
-    y = 15; // This is allowed because y is mutable
-    println!("The new value of y is: {}", y);
+fn is_odd_number(number: u128) -> bool {
+    number % 2 != 0
 }
 
-fn shadowing() {
-    let x = 5;
-    let x = x + 1; // x is shadowed here
-    {
-        let x = x * 2; // x is shadowed again in this inner scope
-        println!("The value of x in the inner scope is: {}", x); // 12
-    }
-    println!("The value of x in the outer scope is: {}", x); // 6
-}
+// fn immutability_test() {
+//     let x   = 1;   
+//     println!("The value of x is: {}", x);
+//     // x = 6; // This line would cause a compile-time error because x is immutable
 
-//Type of data
+//     let mut y = 10; // y is mutable
+//     println!("The value of y is: {}", y);
+//     y = 15; // This is allowed because y is mutable
+//     println!("The new value of y is: {}", y);
+// }
 
-fn scalar_types() {
-    let integer: i32 = 42; // 32-bit signed integer
-    let float: f64 = 3.14; // 64-bit floating point
-    let boolean: bool = true; // Boolean type
-    let character: char = 'R'; // Character type
+// fn shadowing() {
+//     let x = 5;
+//     let x = x + 1; // x is shadowed here
+//     {
+//         let x = x * 2; // x is shadowed again in this inner scope
+//         println!("The value of x in the inner scope is: {}", x); // 12
+//     }
+//     println!("The value of x in the outer scope is: {}", x); // 6
+// }
 
-    println!("Integer: {}, Float: {}, Boolean: {}, Character: {}", integer, float, boolean, character);
-}
+// //Type of data
+
+// fn scalar_types() {
+//     let integer: i32 = 42; // 32-bit signed integer
+//     let float: f64 = 3.14; // 64-bit floating point
+//     let boolean: bool = true; // Boolean type
+//     let character: char = 'R'; // Character type
+
+//     println!("Integer: {}, Float: {}, Boolean: {}, Character: {}", integer, float, boolean, character);
+// }
